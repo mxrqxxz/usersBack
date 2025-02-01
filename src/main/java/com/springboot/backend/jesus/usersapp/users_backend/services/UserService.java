4c51cb96b8be37713143839038daf4fcb420124c
mxrqxxz <4186790@alu.murciaeduca.es>
2025-02-01 19:04:50 +0100

    Servicio implementado

---
+++ src/main/java/com/springboot/backend/jesus/usersapp/users_backend/services/UserService.java
@@ -0,0 +1,17 @@
+package com.springboot.backend.jesus.usersapp.users_backend.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.springboot.backend.jesus.usersapp.users_backend.entities.User;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    User save(User user);
+
+    void deleteById(Long id);
+}

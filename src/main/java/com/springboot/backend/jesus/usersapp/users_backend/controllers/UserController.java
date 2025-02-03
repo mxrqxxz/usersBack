f03e8493bcee4e15ac09d80b5f78fd8d2210d07d
mxrqxxz <4186790@alu.murciaeduca.es>
2025-02-03 02:49:09 +0100

    Controlador añadido y funcionando correctamente

---
+++ src/main/java/com/springboot/backend/jesus/usersapp/users_backend/controllers/UserController.java
@@ -0,0 +1,73 @@
+package com.springboot.backend.jesus.usersapp.users_backend.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.backend.jesus.usersapp.users_backend.entities.User;
+import com.springboot.backend.jesus.usersapp.users_backend.services.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired // tambien se puede poner en el constructor y no hace falta autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> list() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> show(@PathVariable Long id) {
+        Optional<User> userOptional = userService.findById(id);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.ok(userOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {        
+        return ResponseEntity.ok(userService.save(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> userOptional = userService.findById(id);
+        if (userOptional.isPresent()) {
+            User userUpdate = userOptional.get();
+            userUpdate.setName(user.getName());
+            userUpdate.setLastname(user.getLastname());
+            userUpdate.setUsername(user.getUsername());
+            userUpdate.setEmail(user.getEmail());
+            userUpdate.setPassword(user.getPassword());
+            return ResponseEntity.ok(userService.save(userUpdate));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+}
